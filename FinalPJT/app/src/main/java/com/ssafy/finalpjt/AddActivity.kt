ca59771bf54bef8c461603df6b3940ea06a769ed
Kihun SONG <kihunbuzz@naver.com>
2022-11-17 13:45:42 +0900

    #1_toKotlin, AddActivity, AddListFragment

--- FinalPJT/app/src/main/java/com/ssafy/finalpjt/AddActivity.kt
+++ FinalPJT/app/src/main/java/com/ssafy/finalpjt/AddActivity.kt
@@ -1,97 +1,43 @@
 package com.ssafy.finalpjt
 
-import android.app.Activity.RESULT_OK
 import android.os.Bundle
-import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
-import android.widget.ImageButton
 import android.widget.LinearLayout
 import com.ssafy.finalpjt.db.model.Goal
-import com.ssafy.finalpjt.db.DBConst.GoalTable
-import com.ssafy.finalpjt.L
 import com.ssafy.finalpjt.db.model.GoalSub
-import com.ssafy.finalpjt.db.DBConst.SubGoalTable
-import kotlin.Throws
-import com.ssafy.finalpjt.db.DbHelper
-import com.ssafy.finalpjt.db.dao.GoalDAO
-import com.ssafy.finalpjt.db.dao.GoalSubDAO
-import kotlin.jvm.Synchronized
-import com.ssafy.finalpjt.db.CommonDAO
-import com.ssafy.finalpjt.db.DBConst
-import com.ssafy.finalpjt.db.BaseAsyncTask
-import com.ssafy.finalpjt.db.GoalDataTask
-import kotlin.jvm.Volatile
-import com.ssafy.finalpjt.db.GoalSubDataTask
-import com.ssafy.finalpjt.DBHelper
-import com.ssafy.finalpjt.MainActivity
-import com.ssafy.finalpjt.SampleData
-import com.ssafy.finalpjt.MyFragment.MyAdapter
-import com.ssafy.finalpjt.MyFragment
 import androidx.appcompat.app.AppCompatActivity
-import com.ssafy.finalpjt.SubItemView
 import com.ssafy.finalpjt.db.factory.GoalDAOFactory
 import com.ssafy.finalpjt.db.factory.GoalSubDAOFactory
-import androidx.recyclerview.widget.RecyclerView
-import com.ssafy.finalpjt.FragmentMain.RecyclerAdapter
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.ssafy.finalpjt.DetailActivity
-import com.ssafy.finalpjt.AddActivity
-import com.ssafy.finalpjt.FragmentMain.RecyclerAdapter.ItemViewHolder
-import androidx.cardview.widget.CardView
-import com.ssafy.finalpjt.FragmentShop.SingerAdapter
-import com.ssafy.finalpjt.SingerShopItem
-import com.ssafy.finalpjt.SingerViewer
-import com.ssafy.finalpjt.FragmentTodo
-import com.ssafy.finalpjt.AddListFragment
-import androidx.core.content.ContextCompat
-import com.google.android.material.navigation.NavigationView
-import com.ssafy.finalpjt.MainActivity.AlarmHATT
-import com.ssafy.finalpjt.FragmentMain
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.core.view.GravityCompat
-import com.ssafy.finalpjt.FragmentGoals
-import com.ssafy.finalpjt.FragmentShop
-import com.ssafy.finalpjt.FragmentSetting
-import com.ssafy.finalpjt.BroadcastD
-import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
-import com.ssafy.finalpjt.Goals_PagerAdapter
-import com.google.android.material.tabs.TabLayout.ViewPagerOnTabSelectedListener
-import com.google.android.material.tabs.TabLayout.TabLayoutOnPageChangeListener
-import com.ssafy.finalpjt.DetailActivityUpdate
-import com.ssafy.finalpjt.Goals_Sub
-import androidx.fragment.app.FragmentPagerAdapter
-import com.ssafy.finalpjt.Goals_Fragment1
+import com.ssafy.finalpjt.databinding.ActivityAddBinding
 import java.lang.Exception
 import java.util.ArrayList
 
 class AddActivity : AppCompatActivity() {
-    var mSubItemViewList = ArrayList<SubItemView>()
-    var LL: LinearLayout? = null
-    var edtText: EditText? = null
-    //test
+    private lateinit var binding: ActivityAddBinding
+    private lateinit var mLinearLayout: LinearLayout
+    private var mSubItemViewList = ArrayList<SubItemView>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add)
-        edtText = findViewById(R.id.edtGoal)
-        val button1 = findViewById<ImageButton>(R.id.addSubBtn)
-        LL = findViewById(R.id.LL)
+        binding = ActivityAddBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        mLinearLayout = findViewById(R.id.addLinearLayout)
         addSubView()
-        button1.setOnClickListener { addSubView() }
-        findViewById<View>(R.id.btnDone).setOnClickListener(View.OnClickListener {
-            if (isempty(edtText)) {
+        binding.addSubBtn.setOnClickListener { addSubView() }
+        binding.btnDone.setOnClickListener(View.OnClickListener {
+            if (binding.edtGoal.text.isEmpty()) {
                 return@OnClickListener
             }
-            val goalTitle = edtText.getText().toString()
+            val goalTitle = binding.edtGoal.text.toString()
             try {
                 val id = GoalDAOFactory.addGoal(applicationContext, Goal(goalTitle))
                 if (id != -1L) {
                     // 최종목표 db에 성공적으로 저장되었으면 서브 타이틀 db에 데이터를 입력한다.
                     for (subItemView in mSubItemViewList) {
-                        if (!isempty(subItemView.etInput)) {
+                        if (subItemView.etInput!!.text.isNotEmpty()) {
                             GoalSubDAOFactory.addGoalSub(
                                 applicationContext,
                                 GoalSub(id.toString(), subItemView.etInput!!.text.toString(), 0)
@@ -105,10 +51,12 @@ class AddActivity : AppCompatActivity() {
                 e.printStackTrace()
             }
         })
-        findViewById<View>(R.id.btnNo).setOnClickListener { finish() }
+        binding.btnNo.setOnClickListener {
+            finish()
+        }
     }
 
-    val childView: View
+    private val childView: View
         get() {
             val childView =
                 LayoutInflater.from(applicationContext).inflate(R.layout.item_sub_goal_holder, null)
@@ -125,11 +73,6 @@ class AddActivity : AppCompatActivity() {
         val childView = childView
         val etSubGoal = childView.findViewById<View>(R.id.EditText2) as EditText
         mSubItemViewList.add(SubItemView(etSubGoal))
-        LL!!.addView(childView)
-    }
-
-    private fun isempty(et: EditText?): Boolean {
-        return TextUtils.isEmpty(et!!.text.toString()) or et.text.toString()
-            .equals("", ignoreCase = true)
+        mLinearLayout.addView(childView)
     }
 }
